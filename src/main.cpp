23d3622016622423d25348612a18ad0878a6584a
rayman13579 <roman.reno@hotmail.com>
2026-01-05 05:41:04 +0100

    use ESP-DASH for UI, increase available flash app size

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <Arduino.h>
 #include <WiFi.h>
-#include "AsyncTCP.h"
-#include "ESPAsyncWebServer.h"
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <ESPDash.h>
 
-AsyncWebServer server(80);
+
+const int MAX_SENSOR_VALUE = 4095;
 
 const int muxEnable = 1;
 const int muxOutput = 2;
@@ -27,18 +29,41 @@ const int valve2 = 37;
 const int valve3 = 36;
 const int valve4 = 35;
 
+
+AsyncWebServer server(80);
+ESPDash dashboard(server);
+
+
+dash::SeparatorCard separator3(dashboard, "Valve States");
+dash::FeedbackCard valveCard1(dashboard, "Valve 1");
+dash::FeedbackCard valveCard2(dashboard, "Valve 2");
+dash::FeedbackCard valveCard3(dashboard, "Valve 3");
+dash::FeedbackCard valveCard4(dashboard, "Valve 4");
+
+dash::SeparatorCard separator(dashboard, "Soil Moisture");
+dash::HumidityCard<int, 0> moistCard1(dashboard, "Moisture 1");
+dash::HumidityCard<int, 0> moistCard2(dashboard, "Moisture 2");
+dash::HumidityCard<int, 0> moistCard3(dashboard, "Moisture 3");
+dash::HumidityCard<int, 0> moistCard4(dashboard, "Moisture 4");
+
+dash::SeparatorCard separator2(dashboard, "Water Flow");
+dash::HumidityCard<int, 0> flowCard1(dashboard, "Flow 1", "l/min");
+dash::HumidityCard<int, 0> flowCard2(dashboard, "Flow 2", "l/min");
+dash::HumidityCard<int, 0> flowCard3(dashboard, "Flow 3", "l/min");
+dash::HumidityCard<int, 0> flowCard4(dashboard, "Flow 4", "l/min");
+
 void startServer() {
   Serial.println("Connecting to WiFi...");
   WiFi.mode(WIFI_STA);
   WiFi.begin("Wokwi-GUEST", "", 6);
   if (WiFi.waitForConnectResult(5000) != WL_CONNECTED) {
     Serial.println("WiFi Failed!");
-    exit(1);
+    return;
   }
   Serial.print("Connected! IP address: ");
   Serial.println(WiFi.localIP());
 
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+  server.on("/test", HTTP_GET, [](AsyncWebServerRequest *request) {
     request->send(200, "text/plain", "Hello, world");
   });
 
@@ -56,11 +81,12 @@ int readFromInput(int input) {
   digitalWrite(mux2, bitRead(input, 2));
   digitalWrite(mux3, bitRead(input, 3));
   delay(5);
-  return analogRead(muxOutput);
+  int value = analogRead(muxOutput);
+  return (value * 100) / MAX_SENSOR_VALUE;
 }
 
 void openValveIfSoilDry(int sensor, int valve) {
-  if (readFromInput(sensor) < 2000) {
+  if (readFromInput(sensor) < 50) {
     digitalWrite(valve, HIGH);
   } else {
     digitalWrite(valve, LOW);
@@ -117,8 +143,36 @@ void setup() {
   startServer();
 }
 
+void updateValveState(dash::FeedbackCard<> &valveCard, int valvePin) {
+  if (digitalRead(valvePin)) {
+    valveCard.setFeedback("Open", dash::Status::SUCCESS);
+  } else {
+    valveCard.setFeedback("Closed", dash::Status::NONE);
+  }
+
+}
+
+void updateDashboard() {
+  updateValveState(valveCard1, valve1);
+  updateValveState(valveCard2, valve2);
+  updateValveState(valveCard3, valve3);
+  updateValveState(valveCard4, valve4);
+
+  moistCard1.setValue(readFromInput(moist1));
+  moistCard2.setValue(readFromInput(moist2));
+  moistCard3.setValue(readFromInput(moist3));
+  moistCard4.setValue(readFromInput(moist4));
+
+  flowCard1.setValue(readFromInput(flow1));
+  flowCard2.setValue(readFromInput(flow2));
+  flowCard3.setValue(readFromInput(flow3));
+  flowCard4.setValue(readFromInput(flow4));
+
+  dashboard.sendUpdates();
+}
+
 void loop() {
-  delay(10); // this speeds up the simulation
+  delay(50);
 
  // printSensorStats();
 
@@ -126,4 +180,6 @@ void loop() {
   openValveIfSoilDry(moist2, valve2);
   openValveIfSoilDry(moist3, valve3);
   openValveIfSoilDry(moist4, valve4);
+
+  updateDashboard();
 }
