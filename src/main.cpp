18a4961135207c282b768f7d0c3632bf93b08bbd
rayman13579 <roman.reno@hotmail.com>
2026-01-11 14:27:08 +0100

    don't use MUX, flow sensor is not analogue, own partition table to fit app + ota, upload via ota, read log via web

--- src/main.cpp
+++ src/main.cpp
@@ -3,32 +3,34 @@
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <ESPDash.h>
+#include <ElegantOTA.h>
+#include <WebSerial.h>
 
 
-const int MAX_SENSOR_VALUE = 4095;
+//ADC2 can not be used when WiFi enabled
 
-const int muxEnable = 1;
-const int muxOutput = 2;
-const int mux0 = 7;
-const int mux1 = 6;
-const int mux2 = 5;
-const int mux3 = 4;
+const int MAX_DRY_VALUE = 3333;
+const int MIN_DRY_VALUE = 1140;
 
-const int moist1 = 0;
-const int moist2 = 1;
-const int moist3 = 2;
-const int moist4 = 3;
+const int moist1 = 4;
+const int moist2 = 5;
+const int moist3 = 6;
+const int moist4 = 7;
 
-const int flow1 = 15;
-const int flow2 = 14;
-const int flow3 = 13;
-const int flow4 = 12;
+const int flow1 = 10;
+const int flow2 = 9;
+const int flow3 = 3;
+const int flow4 = 8;
 
 const int valve1 = 38;
 const int valve2 = 37;
 const int valve3 = 36;
 const int valve4 = 35;
 
+volatile int flowFrequency1 = 0;
+int flow_l_min_1 = 0;
+unsigned long currentTime;
+unsigned long cloopTime;
 
 AsyncWebServer server(80);
 ESPDash dashboard(server);
@@ -55,7 +57,7 @@ dash::HumidityCard<int, 0> flowCard4(dashboard, "Flow 4", "l/min");
 void startServer() {
   Serial.println("Connecting to WiFi...");
   WiFi.mode(WIFI_STA);
-  WiFi.begin("Wokwi-GUEST", "", 6);
+  WiFi.begin("Wifi", "pw");
   if (WiFi.waitForConnectResult(5000) != WL_CONNECTED) {
     Serial.println("WiFi Failed!");
     return;
@@ -75,18 +77,13 @@ void startServer() {
   Serial.println("HTTP server started");
 }
 
-int readFromInput(int input) {
-  digitalWrite(mux0, bitRead(input, 0));
-  digitalWrite(mux1, bitRead(input, 1));
-  digitalWrite(mux2, bitRead(input, 2));
-  digitalWrite(mux3, bitRead(input, 3));
-  delay(5);
-  int value = analogRead(muxOutput);
-  return (value * 100) / MAX_SENSOR_VALUE;
+int readMoistValue(int input) {
+  int value = analogRead(input);
+  return constrain(map(value, MIN_DRY_VALUE, MAX_DRY_VALUE, 100, 0), 0, 100);
 }
 
 void openValveIfSoilDry(int sensor, int valve) {
-  if (readFromInput(sensor) < 50) {
+  if (readMoistValue(sensor) < 50) {
     digitalWrite(valve, HIGH);
   } else {
     digitalWrite(valve, LOW);
@@ -94,9 +91,7 @@ void openValveIfSoilDry(int sensor, int valve) {
 }
 
 void printSensorStats() {
-  Serial.print("Moist: ");
-  Serial.print(readFromInput(moist1));
-  Serial.print(" | ");
+ /* Serial.print(" | ");
   Serial.print(readFromInput(moist1));
   Serial.print(" | ");
   Serial.print(readFromInput(moist2));
@@ -112,35 +107,7 @@ void printSensorStats() {
   Serial.print(" | ");
   Serial.print(readFromInput(flow4));
   Serial.println();
-}
-
-void setup() {
-  Serial.begin(115200);
-  Serial.println("Hello, ESP32-S3!");
-  pinMode(muxOutput, INPUT);
-  pinMode(muxEnable, OUTPUT);
-  pinMode(mux0, OUTPUT);
-  pinMode(mux1, OUTPUT);
-  pinMode(mux2, OUTPUT);
-  pinMode(mux3, OUTPUT);
-
-  pinMode(valve1, OUTPUT);
-  pinMode(valve2, OUTPUT);
-  pinMode(valve3, OUTPUT);
-  pinMode(valve4, OUTPUT);
-
-  digitalWrite(muxEnable, LOW);
-  digitalWrite(mux0, LOW);
-  digitalWrite(mux1, LOW);
-  digitalWrite(mux2, LOW);
-  digitalWrite(mux3, LOW);
-
-  digitalWrite(valve1, LOW);
-  digitalWrite(valve2, LOW);
-  digitalWrite(valve3, LOW);
-  digitalWrite(valve4, LOW);
-
-  startServer();
+*/
 }
 
 void updateValveState(dash::FeedbackCard<> &valveCard, int valvePin) {
@@ -154,32 +121,84 @@ void updateValveState(dash::FeedbackCard<> &valveCard, int valvePin) {
 
 void updateDashboard() {
   updateValveState(valveCard1, valve1);
-  updateValveState(valveCard2, valve2);
-  updateValveState(valveCard3, valve3);
-  updateValveState(valveCard4, valve4);
+//  updateValveState(valveCard2, valve2);
+//  updateValveState(valveCard3, valve3);
+//  updateValveState(valveCard4, valve4);
 
-  moistCard1.setValue(readFromInput(moist1));
-  moistCard2.setValue(readFromInput(moist2));
-  moistCard3.setValue(readFromInput(moist3));
-  moistCard4.setValue(readFromInput(moist4));
+  moistCard1.setValue(readMoistValue(moist1));
+//  moistCard2.setValue(readMoistValue(moist2));
+//  moistCard3.setValue(readMoistValue(moist3));
+//  moistCard4.setValue(readMoistValue(moist4));
 
-  flowCard1.setValue(readFromInput(flow1));
-  flowCard2.setValue(readFromInput(flow2));
-  flowCard3.setValue(readFromInput(flow3));
-  flowCard4.setValue(readFromInput(flow4));
+  flowCard1.setValue(flow_l_min_1);
+//  flowCard2.setValue(readMoistValue(flow2));
+//  flowCard3.setValue(readMoistValue(flow3));
+//  flowCard4.setValue(readMoistValue(flow4));
 
   dashboard.sendUpdates();
 }
 
-void loop() {
-  delay(50);
+void flowInterrupt1() {
+  flowFrequency1++;
+}
 
- // printSensorStats();
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Hello, ESP32-S3!");
+  pinMode(moist1, INPUT);
+  pinMode(moist2, INPUT);
+  pinMode(moist3, INPUT); 
+  pinMode(moist4, INPUT);
+
+  pinMode(flow1, INPUT);
+  pinMode(flow2, INPUT);
+  pinMode(flow3, INPUT);
+  pinMode(flow4, INPUT);
+
+  pinMode(valve1, OUTPUT);
+  pinMode(valve2, OUTPUT);
+  pinMode(valve3, OUTPUT);
+  pinMode(valve4, OUTPUT);
+
+  digitalWrite(flow1, HIGH);
+  attachInterrupt(flow1, flowInterrupt1, RISING);
+
+  digitalWrite(valve1, LOW);
+  digitalWrite(valve2, LOW);
+  digitalWrite(valve3, LOW);
+  digitalWrite(valve4, LOW);
+
+  startServer();
+  ElegantOTA.begin(&server);
+  WebSerial.begin(&server);
+  
+  sei();
+  currentTime = millis();
+  cloopTime = millis();
+}
+
+void loop() {
+  delay(250);
+  ElegantOTA.loop();
+  WebSerial.loop();
+
+  currentTime = millis();
+  if (currentTime >= (cloopTime + 1000)) {
+    cloopTime = currentTime;
+    flow_l_min_1 = flowFrequency1 / 7.5;
+    WebSerial.print("Flow 1 frequency: ");
+    WebSerial.print(flowFrequency1);
+    WebSerial.print(" | l/min: ");
+    WebSerial.println(flow_l_min_1);
+    flowFrequency1 = 0;
+  }
+
+  printSensorStats();
 
   openValveIfSoilDry(moist1, valve1);
-  openValveIfSoilDry(moist2, valve2);
-  openValveIfSoilDry(moist3, valve3);
-  openValveIfSoilDry(moist4, valve4);
+//  openValveIfSoilDry(moist2, valve2);
+//  openValveIfSoilDry(moist3, valve3);
+//  openValveIfSoilDry(moist4, valve4);
 
   updateDashboard();
 }
