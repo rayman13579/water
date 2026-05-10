601ffc0b7dbf03bb9bc140f1f8a8b0b9dd653c65
rayman13579 <roman.reno@hotmail.com>
2026-05-10 20:41:52 +0200

    use modified dashboard lib, finished dashboard with manual watering, charts, and stats, reorder methods

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,8 @@ const int valve2 = 18;
 const int valve3 = 15;
 const int valve4 = 17;
 
+std::vector<int> valves = {valve1, valve2, valve3, valve4};
+
 volatile int flowFrequency1 = 0;
 volatile int flowFrequency2 = 0;
 volatile int flowFrequency3 = 0;
@@ -34,16 +36,74 @@ int flow_l_min_1 = 0;
 int flow_l_min_2 = 0;
 int flow_l_min_3 = 0;
 int flow_l_min_4 = 0;
-unsigned long currentTime;
-unsigned long cloopTime;
 
 Ticker flowCalculationTicker;
 Ticker moistureChartTicker;
 
+bool doDashboardUpdate = false;
+bool doMoistureChartUpdate = false;
+
+bool doWatering = false;
+int selectedValve = 0;
+int wateringDuration = 1;
+unsigned long wateringEndTime = 0;
+
 AsyncWebServer server(80);
 ESPDashboardPlus dashboard("Water");
 
-void startServer()
+void IRAM_ATTR flowInterrupt1()
+{
+  flowFrequency1 = flowFrequency1 + 1;
+}
+
+void IRAM_ATTR flowInterrupt2()
+{
+  flowFrequency2 = flowFrequency2 + 1;
+}
+
+void IRAM_ATTR flowInterrupt3()
+{
+  flowFrequency3 = flowFrequency3 + 1;
+}
+
+void IRAM_ATTR flowInterrupt4()
+{
+  flowFrequency4 = flowFrequency4 + 1;
+}
+
+void setupPins()
+{
+  pinMode(moist1, INPUT);
+  pinMode(moist2, INPUT);
+  pinMode(moist3, INPUT);
+  pinMode(moist4, INPUT);
+
+  pinMode(flow1, INPUT);
+  pinMode(flow2, INPUT);
+  pinMode(flow3, INPUT);
+  pinMode(flow4, INPUT);
+
+  pinMode(valve1, OUTPUT);
+  pinMode(valve2, OUTPUT);
+  pinMode(valve3, OUTPUT);
+  pinMode(valve4, OUTPUT);
+
+  digitalWrite(flow1, HIGH);
+  digitalWrite(flow2, HIGH);
+  digitalWrite(flow3, HIGH);
+  digitalWrite(flow4, HIGH);
+  attachInterrupt(flow1, flowInterrupt1, RISING);
+  attachInterrupt(flow2, flowInterrupt2, RISING);
+  attachInterrupt(flow3, flowInterrupt3, RISING);
+  attachInterrupt(flow4, flowInterrupt4, RISING);
+
+  digitalWrite(valve1, LOW);
+  digitalWrite(valve2, LOW);
+  digitalWrite(valve3, LOW);
+  digitalWrite(valve4, LOW);
+}
+
+void setupServer()
 {
   Serial.println("Connecting to WiFi...");
   WiFi.mode(WIFI_STA);
@@ -57,13 +117,48 @@ void startServer()
   Serial.println(WiFi.localIP());
 
   server.on("/test", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send(200, "text/plain", "Hello, world"); });
+  { 
+    request->send(200, "text/plain", "Hello, world"); 
+  });
 
   server.onNotFound([](AsyncWebServerRequest *request)
-                    { request->send(404, "text/plain", "Not found"); });
+  { 
+    request->send(404, "text/plain", "Not found");
+  });
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void setupDashboard()
+{
   dashboard.begin(&server, DASHBOARD_HTML_DATA, DASHBOARD_HTML_SIZE, true, true);
 
-  dashboard.addActionButton("restart", "Restart", "Restart", "Restart?", "Restart?", []() {
+  DropdownCard *valveDropdown = dashboard.addDropdownCard("valveDropdown", "Valve", "Select valve");
+  valveDropdown->addOption("0", "Valve 1");
+  valveDropdown->addOption("1", "Valve 2");
+  valveDropdown->addOption("2", "Valve 3");
+  valveDropdown->addOption("3", "Valve 4");
+  valveDropdown->onChange = [](const String &value)
+  {
+    selectedValve = value.toInt();
+    dashboard.updateButtonCard("doWater", "water valve " + String(selectedValve + 1) + " for " + String(wateringDuration) + " seconds");
+  };
+  valveDropdown->setValue("0");
+  SliderCard *durationSlider = dashboard.addSliderCard("durationSlider", "Watering Duration", 1, 60, 1, "sec");
+  durationSlider->onChange = [](int value)
+  { 
+    wateringDuration = value;
+    dashboard.updateButtonCard("doWater", "water valve " + String(selectedValve + 1) + " for " + String(wateringDuration) + " seconds");
+  };
+  dashboard.addButtonCard("doWater", "Water", "water valve 1 for 1 seconds", true, []
+  {
+    dashboard.updateButtonCard("doWater", false);
+    wateringEndTime = millis() + wateringDuration * 1000;
+    doWatering = true;
+    digitalWrite(valves[selectedValve], HIGH);
+  });
+  dashboard.addActionButton("restart", "Restart", "Restart", "Restart?", "Restart?", []()
+  { 
     ESP.restart();
   });
 
@@ -72,37 +167,43 @@ void startServer()
   dashboard.addStatusCard("valve3", "Valve 3", StatusIcon::POWER);
   dashboard.addStatusCard("valve4", "Valve 4", StatusIcon::POWER);
 
-  ChartCard *moistChart1 = dashboard.addChartCard("moistChart1", "Moisture History", ChartType::LINE, 100);
-  moistChart1->setSize(1, 1);
-  ChartCard *moistChart2 = dashboard.addChartCard("moistChart2", "Moisture History", ChartType::LINE, 100);
-  moistChart2->setSize(1, 1);
-  ChartCard *moistChart3 = dashboard.addChartCard("moistChart3", "Moisture History", ChartType::LINE, 100);
-  moistChart3->setSize(1, 1);
-  ChartCard *moistChart4 = dashboard.addChartCard("moistChart4", "Moisture History", ChartType::LINE, 100);
-  moistChart4->setSize(1, 1);
-  //  moistChart->addSeries("Moisture 1", "#3700ffff");
-  //  moistChart->addSeries("Moisture 2", "#00FF00");
-  //  moistChart->addSeries("Moisture 3", "#FFFF00");
-  //  moistChart->addSeries("Moisture 4", "#FF0000");
+  ChartCard *moistChart1 = dashboard.addChartCard("moistChart1", "Moisture 1", "%", ChartType::LINE, 48);
+  moistChart1->setSize(2, 1);
+  ChartCard *moistChart2 = dashboard.addChartCard("moistChart2", "Moisture 2", "%", ChartType::LINE, 48);
+  moistChart2->setSize(2, 1);
+  ChartCard *moistChart3 = dashboard.addChartCard("moistChart3", "Moisture 3", "%", ChartType::LINE, 48);
+  moistChart3->setSize(2, 1);
+  ChartCard *moistChart4 = dashboard.addChartCard("moistChart4", "Moisture 4", "%", ChartType::LINE, 48);
+  moistChart4->setSize(2, 1);
 
-  dashboard.addStatCard("moist1", "Moisture 1", "%");
-  dashboard.addStatCard("moist2", "Moisture 2", "%");
-  dashboard.addStatCard("moist3", "Moisture 3", "%");
-  dashboard.addStatCard("moist4", "Moisture 4", "%");
+  dashboard.addStatCard("flow1", "Flow 1", "0", "l/min");
+  dashboard.addStatCard("flow2", "Flow 2", "0", "l/min");
+  dashboard.addStatCard("flow3", "Flow 3", "0", "l/min");
+  dashboard.addStatCard("flow4", "Flow 4", "0", "l/min");
 
-  dashboard.addStatCard("flow1", "Flow 1", "l/min");
-  dashboard.addStatCard("flow2", "Flow 2", "l/min");
-  dashboard.addStatCard("flow3", "Flow 3", "l/min");
-  dashboard.addStatCard("flow4", "Flow 4", "l/min");
-
-  dashboard.addGroup("stuff", "Stuff", {"restart"});
+  dashboard.addGroup("stuff", "Stuff", {"valveDropdown", "durationSlider", "doWater", "restart"});
   dashboard.addGroup("valve", "Valve States", {"valve1", "valve2", "valve3", "valve4"});
   dashboard.addGroup("moistureChart", "Soil Moisture", {"moistChart1", "moistChart2", "moistChart3", "moistChart4"});
-  dashboard.addGroup("moisture", "Soil Moisture", {"moist1", "moist2", "moist3", "moist4"});
   dashboard.addGroup("flow", "Water Flow", {"flow1", "flow2", "flow3", "flow4"});
+}
 
-  server.begin();
-  Serial.println("HTTP server started");
+void calculateFlow()
+{
+  flow_l_min_1 = flowFrequency1 / 7.5;
+  flow_l_min_2 = flowFrequency2 / 7.5;
+  flow_l_min_3 = flowFrequency3 / 7.5;
+  flow_l_min_4 = flowFrequency4 / 7.5;
+  flowFrequency1 = 0;
+  flowFrequency2 = 0;
+  flowFrequency3 = 0;
+  flowFrequency4 = 0;
+
+  /*
+  openValveIfSoilDry(moist1, valve1);
+  openValveIfSoilDry(moist2, valve2);
+  openValveIfSoilDry(moist3, valve3);
+  openValveIfSoilDry(moist4, valve4);
+*/
 }
 
 int readMoistValue(int input)
@@ -142,55 +243,11 @@ void updateDashboard()
   updateValveState(valve3, "valve3");
   updateValveState(valve4, "valve4");
 
-  dashboard.updateStatCard("moist1", String(readMoistValue(moist1)));
-  dashboard.updateStatCard("moist2", String(readMoistValue(moist2)));
-  dashboard.updateStatCard("moist3", String(readMoistValue(moist3)));
-  dashboard.updateStatCard("moist4", String(readMoistValue(moist4)));
-
+  calculateFlow();
   dashboard.updateStatCard("flow1", String(flow_l_min_1));
   dashboard.updateStatCard("flow2", String(flow_l_min_2));
   dashboard.updateStatCard("flow3", String(flow_l_min_3));
   dashboard.updateStatCard("flow4", String(flow_l_min_4));
-}
-
-void IRAM_ATTR flowInterrupt1()
-{
-  flowFrequency1 = flowFrequency1 + 1;
-}
-
-void IRAM_ATTR flowInterrupt2()
-{
-  flowFrequency2 = flowFrequency2 + 1;
-}
-
-void IRAM_ATTR flowInterrupt3()
-{
-  flowFrequency3 = flowFrequency3 + 1;
-}
-
-void IRAM_ATTR flowInterrupt4()
-{
-  flowFrequency4 = flowFrequency4 + 1;
-}
-
-void calculateFlow()
-{
-  flow_l_min_1 = flowFrequency1 / 7.5;
-  flow_l_min_2 = flowFrequency2 / 7.5;
-  flow_l_min_3 = flowFrequency3 / 7.5;
-  flow_l_min_4 = flowFrequency4 / 7.5;
-  dashboard.logInfo("Flow frequencies: " + String(flow_l_min_1) + " | " + String(flow_l_min_2) + " | " + String(flow_l_min_3) + " | " + String(flow_l_min_4));
-  flowFrequency1 = 0;
-  flowFrequency2 = 0;
-  flowFrequency3 = 0;
-  flowFrequency4 = 0;
-
-  openValveIfSoilDry(moist1, valve1);
-  openValveIfSoilDry(moist2, valve2);
-  openValveIfSoilDry(moist3, valve3);
-  openValveIfSoilDry(moist4, valve4);
-
-  updateDashboard();
 }
 
 void updateMoistureCharts()
@@ -201,46 +258,29 @@ void updateMoistureCharts()
   dashboard.updateChartCard("moistChart4", readMoistValue(moist4));
 }
 
+void flowCalculationTickerCallback()
+{
+  doDashboardUpdate = true;
+}
+
+void moistureChartTickerCallback()
+{
+  doMoistureChartUpdate = true;
+}
+
 void setup()
 {
   Serial.begin(9600);
-  Serial.println("Hello, ESP32-S3!");
+  setupPins();
+  setupServer();
+  setupDashboard();
 
-  pinMode(moist1, INPUT);
-  pinMode(moist2, INPUT);
-  pinMode(moist3, INPUT);
-  pinMode(moist4, INPUT);
-
-  pinMode(flow1, INPUT);
-  pinMode(flow2, INPUT);
-  pinMode(flow3, INPUT);
-  pinMode(flow4, INPUT);
-
-  pinMode(valve1, OUTPUT);
-  pinMode(valve2, OUTPUT);
-  pinMode(valve3, OUTPUT);
-  pinMode(valve4, OUTPUT);
-
-  digitalWrite(flow1, HIGH);
-  digitalWrite(flow2, HIGH);
-  digitalWrite(flow3, HIGH);
-  digitalWrite(flow4, HIGH);
-  attachInterrupt(flow1, flowInterrupt1, RISING);
-  attachInterrupt(flow2, flowInterrupt2, RISING);
-  attachInterrupt(flow3, flowInterrupt3, RISING);
-  attachInterrupt(flow4, flowInterrupt4, RISING);
-
-  digitalWrite(valve1, LOW);
-  digitalWrite(valve2, LOW);
-  digitalWrite(valve3, LOW);
-  digitalWrite(valve4, LOW);
-
-  startServer();
-
-  flowCalculationTicker.attach(10, calculateFlow);
-  moistureChartTicker.attach(3600, updateMoistureCharts);
+  flowCalculationTicker.attach_ms(250, flowCalculationTickerCallback);
+  moistureChartTicker.attach(10, moistureChartTickerCallback);
   calculateFlow();
   updateMoistureCharts();
+  dashboard.updateDropdownCard("valveDropdown", "0");
+  dashboard.updateSliderCard("durationSlider", 1);
 
   sei();
 }
@@ -248,5 +288,29 @@ void setup()
 void loop()
 {
   dashboard.loop();
-  delay(10);
+
+  if (doDashboardUpdate)
+  {
+    noInterrupts();
+    doDashboardUpdate = false;
+    interrupts();
+    updateDashboard();
+  }
+
+  if (doMoistureChartUpdate)
+  {
+    noInterrupts();
+    doMoistureChartUpdate = false;
+    interrupts();
+    updateMoistureCharts();
+  }
+
+  if (doWatering && millis() >= wateringEndTime)
+  {
+    dashboard.updateButtonCard("doWater", true);
+    digitalWrite(valves[selectedValve], LOW);
+    doWatering = false;
+  }
+
+  delay(150);
 }
